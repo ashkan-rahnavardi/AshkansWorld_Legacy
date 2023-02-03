ae535cc93a410be5c17db6e4ad9a06865152a781
ashkan-rahnavardi <ashkan.rahnavardi@gmail.com>
2023-02-02 21:20:30 -0800

    Finalized Project Summaries

--- src/pages/Components/ProjectCard.jsx
+++ src/pages/Components/ProjectCard.jsx
@@ -7,12 +7,27 @@ import React from 'react';
 import { TechUsed } from './../Components/TechUsed.jsx';
 
 export default function ProjectCard({ project, index, navigate }) {
+	let handleClick = () => {
+		navigate(project.path);
+	};
+
+	let preview = <h2>View Project {String.fromCharCode(8594)}</h2>;
+
+	switch (project.title) {
+		case 'Hangado':
+			handleClick = () => {};
+			preview = <h2>Coming Soon</h2>;
+		case 'ChatBot':
+			handleClick = () => {};
+			preview = <h2>Coming Soon</h2>;
+	}
+
 	return (
 		<Grid item xs={12} md={6} display="flex" key={index}>
 			<Card className="project-card">
 				<CardActionArea
 					onClick={() => {
-						navigate(project.path);
+						handleClick();
 					}}
 				>
 					<CardContent className="project-card-content">
@@ -34,7 +49,7 @@ export default function ProjectCard({ project, index, navigate }) {
 								</ul>
 							</Grid>
 						</Box>
-						<h2>View Preview {String.fromCharCode(8594)}</h2>
+						{preview}
 					</CardContent>
 				</CardActionArea>
 			</Card>
